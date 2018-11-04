e4f4cdcc44f7befe34c85754eca52a7f3c0c2838
Oliver Letterer <oliver.letterer@gmail.com>
2018-11-04 13:55:41 +0100

    Introducing new di runtime

--- pieces/di/runtime.h
+++ pieces/di/runtime.h
@@ -7,12 +7,38 @@
 
 #import <Foundation/Foundation.h>
 
+@protocol RIBDependencyContainer;
+
 NS_ASSUME_NONNULL_BEGIN
 
-BOOL rib_classIsBlacklisted(Class klass);
+#pragma mark - runtime helpers
 
-BOOL rib_canResolveDependency(id object, NSString *property);
+// YES if the property is dynamically implemented
+BOOL rib_propertyIsDynamic(Class klass, NSString *property);
 
-id rib_resolveDependency(id object, NSString *property);
+// Returns the objc encoded type of the property
+NSString *rib_propertyCopyType(Class klass, NSString *property);
+
+// YES iff klass or one of its super classes conforms to protocol
+BOOL rib_classConformsToProtocol(Class klass, Protocol *protocol);
+
+#pragma mark - dependency resolution
+
+// Navigates the dependeny tree upwards to find the dependency from a parent
+id _Nullable rib_resolveDependency(id<RIBDependencyContainer> object, NSString *property, NSString *type);
+
+// Implements a custom setter to notify the dependency graph about the change
+BOOL rib_implementDependencyObserver(Class klass, NSString *dependency);
+
+// Navigates the dependency tree and informs listening children about changes
+void rib_dependencyDidChange(id<RIBDependencyContainer> object, NSString *property);
+
+#pragma mark - property injection
+
+// Enumerates the class list and implements all properties that can by injected
+void rib_implementInjectedPropertiesInImage(NSString *image);
+
+// Implements a single injected property
+BOOL rib_implementInjectedProperty(Class klass, NSString *property);
 
 NS_ASSUME_NONNULL_END
--- pieces/di/runtime.m
+++ pieces/di/runtime.m
@@ -6,120 +6,239 @@
 //
 
 #import "runtime.h"
-#import "RIBDependencyInjection.h"
+
+#import "RIBDependencyGraph.h"
+#import "RIBDependencyContainer.h"
 
 #import <objc/runtime.h>
 
-id _rib_resolveDependency(id object, Class klass, NSString *property, BOOL throwOnNotFound);
-BOOL _rib_canResolveDependency(id object, Class klass, NSString *property);
+@interface RIBDependencyGraph (runtime)
 
-BOOL rib_classIsBlacklisted(Class klass)
+- (BOOL)object:(id<RIBDependencyContainer>)object listensForDependency:(NSString *)dependency;
+- (BOOL)object:(id<RIBDependencyContainer>)object implementsDependency:(NSString *)dependency ofType:(NSString *)type;
+
+- (NSArray<id<RIBDependencyContainer>> *)childrenForObject:(id)object;
+- (NSArray<id<RIBDependencyContainer>> *)parentsForObject:(id)object;
+
+@end
+
+@implementation RIBDependencyGraph (runtime)
+
+- (BOOL)object:(id<RIBDependencyContainer>)object listensForDependency:(NSString *)dependency
 {
-    NSBundle *bundle = [NSBundle bundleForClass:klass];
-    return [bundle.bundlePath.lowercaseString containsString:@"system"];
+    return rib_propertyIsDynamic(object_getClass(object), dependency) && [self containsDependency:dependency type:rib_propertyCopyType(object_getClass(object), dependency)];
 }
 
-BOOL rib_canResolveDependency(id object, NSString *property)
+- (BOOL)object:(id<RIBDependencyContainer>)object implementsDependency:(NSString *)dependency ofType:(NSString *)type
 {
-    return _rib_canResolveDependency(object, object_getClass(object), property);
-}
-
-BOOL _rib_canResolveDependency(id object, Class klass, NSString *property)
-{
-    if (rib_classIsBlacklisted(klass)) {
+    objc_property_t objcProperty = class_getProperty(object_getClass(object), dependency.UTF8String);
+    if (objcProperty == NULL) {
         return NO;
     }
     
-    NSDictionary<NSString *, NSArray<NSString *> *> *dependencyGraph = RIBDependencyInjection.dependencyGraph;
-    
-    objc_property_t objcProperty = class_getProperty(klass, property.UTF8String);
-    if (objcProperty != NULL) {
-        char *rawType = property_copyAttributeValue(objcProperty, "T");
-        char *rawDynamic = property_copyAttributeValue(objcProperty, "D");
-        
-        if (rawDynamic != NULL) {
-            free(rawDynamic);
-        }
-        
-        if (rawType != NULL && rawType[0] == '@') {
-            free(rawType);
-            
-            if (rawDynamic == NULL) {
-                return YES;
-            }
-        }
-    }
-    
-    for (NSString *parentProperty in dependencyGraph[NSStringFromClass(klass)]) {
-        id parent = [object valueForKey:parentProperty];
-        
-        if (parent != nil && _rib_canResolveDependency(parent, object_getClass(parent), property)) {
-            return YES;
-        }
-    }
-    
-    Class superKlass = class_getSuperclass(klass);
-    if (superKlass != Nil) {
-        if (_rib_canResolveDependency(object, superKlass, property)) {
-            return YES;
-        }
-    }
-    
-    return NO;
+    return !rib_propertyIsDynamic(object_getClass(object), dependency) && [rib_propertyCopyType(object_getClass(object), dependency) isEqualToString:type];
 }
 
-id rib_resolveDependency(id object, NSString *property)
+- (NSArray<id<RIBDependencyContainer>> *)childrenForObject:(id)object
 {
-    NSString *klass = NSStringFromClass([object class]);
-    return _rib_resolveDependency(object, object_getClass(object), property, RIBDependencyInjection.dependencyGraph[klass].count == 0);
+    NSMutableArray *result = [NSMutableArray array];
+    void(^appendChildren)(id children) = ^(id children) {
+        if ([children isKindOfClass:NSArray.class]) {
+            [result addObjectsFromArray:children];
+        } else {
+            [result addObject:children];
+        }
+    };
+    
+    Class klass = object_getClass(object);
+    while (rib_classConformsToProtocol(klass, @protocol(RIBDependencyContainer))) {
+        for (NSString *child in self.graph[NSStringFromClass(klass)].children) {
+            appendChildren([object valueForKey:child]);
+        }
+        
+        klass = class_getSuperclass(klass);
+    }
+    
+    return result;
 }
 
-id _rib_resolveDependency(id object, Class klass, NSString *property, BOOL throwOnNotFound)
+- (NSArray<id<RIBDependencyContainer>> *)parentsForObject:(id)object
 {
-    NSDictionary<NSString *, NSArray<NSString *> *> *dependencyGraph = RIBDependencyInjection.dependencyGraph;
+    NSMutableArray *result = [NSMutableArray array];
+    void(^appendChildren)(id children) = ^(id children) {
+        if ([children isKindOfClass:NSArray.class]) {
+            [result addObjectsFromArray:children];
+        } else {
+            [result addObject:children];
+        }
+    };
     
+    Class klass = object_getClass(object);
+    while (rib_classConformsToProtocol(klass, @protocol(RIBDependencyContainer))) {
+        if (self.graph[NSStringFromClass(klass)].parent != nil) {
+            appendChildren([object valueForKey:self.graph[NSStringFromClass(klass)].parent]);
+        }
+        
+        klass = class_getSuperclass(klass);
+    }
+    
+    return result;
+}
+
+@end
+
+#pragma mark - runtime helpers
+
+BOOL rib_propertyIsDynamic(Class klass, NSString *property)
+{
     objc_property_t objcProperty = class_getProperty(klass, property.UTF8String);
-    if (objcProperty != NULL) {
-        char *rawType = property_copyAttributeValue(objcProperty, "T");
-        char *rawDynamic = property_copyAttributeValue(objcProperty, "D");
-        
-        if (rawDynamic != NULL) {
-            free(rawDynamic);
+    assert(objcProperty != NULL);
+    
+    char *dynamic = property_copyAttributeValue(objcProperty, "D");
+    if (dynamic != NULL) { free(dynamic); }
+    
+    return dynamic != NULL;
+}
+
+NSString *rib_propertyCopyType(Class klass, NSString *property)
+{
+    objc_property_t objcProperty = class_getProperty(klass, property.UTF8String);
+    assert(objcProperty != NULL);
+    
+    char *type = property_copyAttributeValue(objcProperty, "T");
+    assert(type != NULL);
+    
+    NSString *stringedType = [[NSString alloc] initWithUTF8String:type];
+    free(type);
+    
+    return stringedType;
+}
+
+BOOL rib_classConformsToProtocol(Class klass, Protocol *protocol)
+{
+    Class foundClass = klass;
+    
+    while (foundClass != Nil) {
+        if (class_conformsToProtocol(foundClass, @protocol(RIBDependencyContainer))) {
+            break;
         }
         
-        if (rawType != NULL && rawType[0] == '@') {
-            free(rawType);
-            
-            if (rawDynamic == NULL) {
-                return [object valueForKey:property];
+        foundClass = class_getSuperclass(foundClass);
+    }
+    
+    return foundClass != Nil;
+}
+
+#pragma mark - dependency resolution
+
+void rib_dependencyDidChange(id<RIBDependencyContainer> object, NSString *property)
+{
+    RIBDependencyGraph *deprendencyGraph = [object.class dependencyGraph];
+    
+    if ([deprendencyGraph object:object listensForDependency:property]) {
+        [object rib_injectedDependencyDidChange:property];
+    }
+    
+    for (id<RIBDependencyContainer> child in [deprendencyGraph childrenForObject:object]) {
+        rib_dependencyDidChange(child, property);
+    }
+}
+
+id _Nullable _rib_resolveDependency(id<RIBDependencyContainer> object, NSString *property, NSString *type, RIBDependencyGraph *deprendencyGraph, BOOL *found)
+{
+    if ([deprendencyGraph object:object implementsDependency:property ofType:type]) {
+        if (found != NULL) {
+            *found = YES;
+        }
+        
+        return [(id)object valueForKey:property];
+    }
+    
+    for (id<RIBDependencyContainer> parent in [deprendencyGraph parentsForObject:object]) {
+        BOOL didFind = NO;
+        id value = _rib_resolveDependency(parent, property, type, deprendencyGraph, &didFind);
+        
+        if (didFind) {
+            if (found != NULL) {
+                *found = YES;
             }
-        }
-    }
-    
-    for (NSString *parentProperty in dependencyGraph[NSStringFromClass(klass)]) {
-        id parent = [object valueForKey:parentProperty];
-        
-        if (parent != nil) {
-            id result = _rib_resolveDependency(parent, object_getClass(parent), property, NO);
             
-            if (result != nil) {
-                return result;
-            }
+            return value;
         }
-    }
-    
-    Class superKlass = class_getSuperclass(klass);
-    if (superKlass != Nil) {
-        id result = _rib_resolveDependency(object, superKlass, property, YES);
-        
-        if (result != nil) {
-            return result;
-        }
-    }
-    
-    if (throwOnNotFound) {
-        assert(false);
     }
     
     return nil;
+}
+
+id _Nullable rib_resolveDependency(id<RIBDependencyContainer> object, NSString *property, NSString *type)
+{
+    return _rib_resolveDependency(object, property, rib_propertyCopyType(object_getClass(object), property), [object.class dependencyGraph], NULL);
+}
+
+BOOL rib_implementDependencyObserver(Class klass, NSString *dependency)
+{
+    assert([rib_propertyCopyType(klass, dependency) hasPrefix:@"@"]);
+    
+    SEL setter = NSSelectorFromString([NSString stringWithFormat:@"set%@%@:", [dependency substringToIndex:1].uppercaseString, [dependency substringFromIndex:1]]);
+    IMP originalImplementation = method_getImplementation(class_getInstanceMethod(klass, setter));
+    
+    IMP swizzledImplementation = imp_implementationWithBlock(^(id self, id value) {
+        id oldValue = [self valueForKey:dependency];
+        ((void(*)(id, SEL, id))originalImplementation)(self, setter, value);
+        id newValue = [self valueForKey:dependency];
+        
+        if (oldValue == newValue || [oldValue isEqual:newValue]) {
+            return;
+        }
+        
+        rib_dependencyDidChange(self, dependency);
+    });
+    
+    return class_replaceMethod(klass, setter, swizzledImplementation, "v@:@");
+}
+
+#pragma mark - property injection
+
+void rib_implementInjectedPropertiesInImage(NSString *image)
+{
+    unsigned int count = 0;
+    const char **classes = objc_copyClassNamesForImage(image.UTF8String, &count);
+    
+    for (unsigned int i = 0; i < count; i++) {
+        Class klass = NSClassFromString(@(classes[i]));
+
+        if (!rib_classConformsToProtocol(klass, @protocol(RIBDependencyContainer))) {
+            continue;
+        }
+        
+        RIBDependencyGraph *dependencyGraph = [klass dependencyGraph];
+        
+        unsigned int count = 0;
+        objc_property_t *properties = class_copyPropertyList(klass, &count);
+        
+        for (unsigned int i = 0; i < count; i++) {
+            NSString *property = @(property_getName(properties[i]));
+            
+            if (rib_propertyIsDynamic(klass, property) && [dependencyGraph containsDependency:property type:rib_propertyCopyType(klass, property)]) {
+                rib_implementInjectedProperty(klass, property);
+            }
+        }
+    }
+}
+
+BOOL rib_implementInjectedProperty(Class klass, NSString *property)
+{
+    assert(rib_classConformsToProtocol(klass, @protocol(RIBDependencyContainer)));
+    
+    NSString *type = rib_propertyCopyType(klass, property);
+    assert(rib_propertyIsDynamic(klass, property));
+    
+    RIBDependencyGraph *dependencyGraph = [klass dependencyGraph];
+    assert([dependencyGraph containsDependency:property type:type]);
+    
+    IMP implementation = imp_implementationWithBlock(^id(id self) {
+        return rib_resolveDependency(self, property, type);
+    });
+    
+    return class_addMethod(klass, NSSelectorFromString(property), implementation, "@@:");
 }
