e839fdd6f1a8725f996645eb7c3c431d5c588e00
Oliver Letterer <oliver.letterer@gmail.com>
2018-11-22 10:07:36 +0100

    Updating demo app

--- demo/AppDelegate.m
+++ demo/AppDelegate.m
@@ -44,60 +44,9 @@
     _router = [RootBuilder buildWithEnvironment:application.applicationEnvironment];
     [_router launch:self.window];
 
-    [self _debugStreams:NO];
-    [self _debugRouterHierarchy:NO];
+    [self _debugRouterHierarchy:YES];
     
     return YES;
-}
-
-- (void)_debugStreams:(BOOL)enabled
-{
-    if (!enabled) {
-        return;
-    }
-    
-    self.streamObserver = [[StreamOwner alloc] init];
-    RIBStream<NSNumber *> *stream1 = [[RIBStream alloc] initWithInitialValue:@5];
-    RIBStream<NSNumber *> *stream2 = [[RIBStream alloc] initWithInitialValue:@10];
-    
-    NSLog(@"stream1 = %@", stream1);
-    NSLog(@"stream2 = %@", stream2);
-    
-    [[stream1 subscribeNext:^(NSNumber * _Nullable next) {
-        NSLog(@"stream1 has now value %@", next);
-    }] addToBag:self.streamObserver.disposableBag];
-    
-    [[stream2 subscribeNext:^(NSNumber * _Nullable next) {
-        NSLog(@"stream2 has now value %@", next);
-    }] addToBag:self.streamObserver.disposableBag];
-    
-    RIBStream<NSNumber *> *stream3 = [RIBStream reduce:stream1 stream:stream2 block:^id _Nullable(NSNumber * _Nullable object1, NSNumber * _Nullable object2) {
-        return @(object1.integerValue + object2.integerValue);
-    }];
-    
-    NSLog(@"stream3 = %@", stream3);
-    
-    [[stream3 subscribeNext:^(NSNumber * _Nullable next) {
-        NSLog(@"stream3 has now value %@", next);
-    }] addToBag:self.streamObserver.disposableBag];
-    
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [stream1 postNextValue:@10];
-    });
-    
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [stream2 postNextValue:@20];
-    });
-    
-    [NSTimer scheduledTimerWithTimeInterval:5.0 repeats:NO block:^(NSTimer * _Nonnull timer) {
-        NSLog(@"Deallocation stream owner after 5s");
-        self.streamObserver = nil;
-    }];
-    
-    [NSTimer scheduledTimerWithTimeInterval:6.0 repeats:NO block:^(NSTimer * _Nonnull timer) {
-        NSLog(@"Posting new value to stream1, which should not trigger any observers");
-        [stream1 postNextValue:@10];
-    }];
 }
 
 - (void)_debugRouterHierarchy:(BOOL)enabled
